86800a159b7cbfafff707a2d3fbd875583234f2c
varsey <eugeny.varseev@gmail.com>
2020-02-14 17:43:23 +0300

    web json functionality

--- venv/talivandr.py
+++ venv/talivandr.py
@@ -1,9 +1,18 @@
 import json
+import requests
 from difflib import get_close_matches
+#getting data online
+r = requests.get('http://book40.hostenko.com/tun/dictionary.json')
+data = r.json()
+# data = json.load(open("dictionary.json")) #for local
 
-data = json.load(open("dictionary.json"))
+# def function(json_object, name):
+#    for dict in json_object:
+#        if dict == name:
+#            return dict['Translation']
 
 def retrive_definition(word):
+
     word = word.lower()
 
     if word in data:
@@ -13,7 +22,7 @@ def retrive_definition(word):
     elif word.upper() in data:
         return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
-        self.comment_box.AppendText("We found %s instead: " % get_close_matches(word, data.keys())[0]) #HOW TO OUTPU???
+        print("We found %s instead: " % get_close_matches(word, data.keys())[0]) #HOW TO OUTPU???
         return data[get_close_matches(word, data.keys())[0]]
 
 import wx
@@ -21,17 +30,11 @@ class MyFrame(wx.Frame):
     def __init__(self, parent, title):
 #создаем не разворачивающееся окно
         wx.Frame.__init__(self, None, -1, 'pyTalivandr 2.0 Beta', size = (620, 430), style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
-#сквозная вспомогательная переменная-счетчик
-        self.index = 0
-#        self.state = ''
+
 #элементы интерфейса
         self.panel = wx.Panel(self)
         self.search_box = wx.TextCtrl(self.panel, pos = (20, 10), size = (450, 20), style=wx.TE_PROCESS_ENTER)
         self.search_box.SetValue('')
-#        self.search_box.Bind(wx.EVT_TEXT, self.TextChanged, self.search_box)
-#        self.search_box.Bind(wx.EVT_TEXT_ENTER, self.TextChanged, self.search_box) #style=wx.TE_PROCESS_ENTER
-#вывод текста для отладки
-#        self.text_box = wx.TextCtrl(self.panel, pos = (20, 350), size = (300, 200), style=wx.TE_MULTILINE|wx.TE_READONLY)
         self.comment_box = wx.TextCtrl(self.panel, pos=(20, 40), size=(550, 300), style=wx.TE_MULTILINE | wx.TE_READONLY)
 #кнопки
         self.search_button = wx.Button(self.panel, label = 'SEARCH', style=wx.ALIGN_CENTER, pos=(480,10),size = (50, 20))
@@ -51,6 +54,7 @@ class MyFrame(wx.Frame):
         self.comment_box.SetValue('')
         word_user = self.search_box.GetValue()
         output = retrive_definition(word_user)
+#        output = function(data, word_user)
 
         if type(output) == list:
             for item in output:
