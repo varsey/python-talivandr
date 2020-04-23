c9341279a9cdb8e99e2309f3ef77340af8472d83
varsey <eugeny.varseev@gmail.com>
2020-04-23 16:31:53 +0300

    - search results update fix - requirements fix

--- src/modules/design.py
+++ src/modules/design.py
@@ -11,22 +11,22 @@ class Mywin(wx.Frame):
 
         panel_main = wx.Panel(self)
         self.search_box = wx.TextCtrl(panel_main, 2, style=wx.TE_PROCESS_ENTER)
-        self.search_box.SetDefaultStyle(wx.TextAttr(wx.NullColour, wx.BLUE))
-        self.lst1 = wx.html.SimpleHtmlListBox (panel_main, style=wx.LB_SINGLE)
-        self.lst2 = wx.html.SimpleHtmlListBox (panel_main, -1, style=wx.LB_SINGLE)
-        self.label_header = wx.html.HtmlWindow(panel_main, style=wx.TE_READONLY |
+#        self.search_box.SetDefaultStyle(wx.TextAttr(wx.NullColour, wx.BLUE))
+        self.results_list = wx.html.SimpleHtmlListBox(panel_main, -1, style=wx.LB_SINGLE | wx.SUNKEN_BORDER)
+#        self.lst2 = wx.html.SimpleHtmlListBox (panel_main, -1, style=wx.LB_SINGLE)
+        self.comments_section = wx.html.HtmlWindow(panel_main, style=wx.TE_READONLY |
                                                            wx.TE_MULTILINE | wx.TE_NO_VSCROLL | wx.BORDER_NONE)
 
         box = wx.BoxSizer(wx.VERTICAL)
 
         box.Add(self.search_box, 0, wx.EXPAND)
-        box.Add(self.lst1, 1, wx.EXPAND)
-        box.Add(self.label_header, 2, wx.EXPAND)
+        box.Add(self.results_list, 1, wx.EXPAND)
+        box.Add(self.comments_section, 2, wx.EXPAND)
 
         panel_main.SetSizer(box)
 
         self.Centre()
-        self.Bind(wx.EVT_LISTBOX, self.onListBox, self.lst1)
+        self.Bind(wx.EVT_LISTBOX, self.onListBox, self.results_list)
         self.Bind(wx.EVT_TEXT_ENTER, self.Txt_Ent, id=2)
 
         self.makeMenuBar()
@@ -62,32 +62,30 @@ class Mywin(wx.Frame):
 ###MENU DESCRIPTIONS ENDS HERE###########
 
     def Txt_Ent(self, event):
-        self.search_button_click(event)
+        self.searching(self.search_box.GetValue())
 
     def onListBox(self, event):
-#        self.text.Clear()
-        self.label_header.ClearBackground()
+        self.comments_section.SetPage('')
+        self.comments_section.ClearBackground()
 
-        index = event.GetEventObject().GetSelection()
+        index = self.results_list.GetSelection() #event.GetEventObject().GetSelection()
         search_result = self.searching(self.search_box.GetValue())
+        self.results_list.SetSelection(index)
+        self.comments_section.SetPage(search_result[index])
+        search_result.clear()
 
-        self.label_header.SetPage(search_result[index])
-#        self.text.AppendText(search_result[index])
-
-#========
-
-    def search_button_click(self, event):
-        self.search_box.SetValue('')
-        search = self.search_box.GetValue()
-        self.searching(search)
+#================================================================================#
 
     def searching(self, str):
         global output_str
+        notfound = True
+
+        self.results_list.Set(())
 
         output_str = list()
-        notfound = True
+        output_str.clear()
+
         str = str.lower()
-        self.lst1.Clear()
 
         if (len(str)) >= 3:
             for x in range(len(modules.config.data)):
@@ -95,18 +93,18 @@ class Mywin(wx.Frame):
 #                    output_str.clear()
                 if str in modules.config.data[x]['Term'] or str in modules.config.data[x]['Translation']:
                     notfound = False
-                    self.lst1.Append(
-                        modules.config.data[x]['Term'] + ' - ' + modules.config.data[x]['Translation'])
-                    output_str.append('<h5>' +modules.config.data[x]['Term'] + ' - '
+                    self.results_list.AppendItems(modules.config.data[x]['Term'] + ' - ' + modules.config.data[x]['Translation'])
+                    output_str.append('<h5>' + modules.config.data[x]['Term'] + ' - '
                                       + modules.config.data[x]['Translation'] + '</h5><br /><br />'
                                       + modules.config.data[x]['Comment'])
 
         else:
 #            self.text.AppendText('Search string is too short')
-            self.label_header.SetPage('Search string is too short')
+            self.comments_section.SetPage('Search string is too short')
 
         if notfound == True and (len(str)) >= 3:
 #            self.text.AppendText('Nothing found')
-            self.label_header.SetPage('Nothing found. Please try again')
+            self.comments_section.SetPage('Nothing found. Please try again')
 
+        str = ''
         return output_str
